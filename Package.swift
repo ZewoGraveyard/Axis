6203bdd2b6b120f6e0aa9af8ea8f2085a65448ae
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-20 05:08:04 -0800

    fix Package file

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-	name: "URI",
+	name: "Core",
 	dependencies: [
 		.Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1)
 	]
