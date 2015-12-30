7cee940c062c28947589dbeff124770f754ea365
Alex Studnička <AStudnicka@gmail.com>
2015-12-30 15:51:41 +0100

    Fixed #10

--- Sources/Core/Base64/Base64.swift
+++ Sources/Core/Base64/Base64.swift
@@ -118,7 +118,7 @@ private class Base64Decoder {
 	static let decoding: [Int8] = [62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -2, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51]
 
 	static func decodeValue(value: UInt8) -> Int8 {
-		let tmp = Int(value - 43)
+		let tmp = Int(value) - 43
 		guard tmp >= 0 && tmp < Base64Decoder.decoding.count else { return -1 }
 		return Base64Decoder.decoding[tmp]
 	}
@@ -144,7 +144,7 @@ private class Base64Decoder {
 			guard offset < bytes.count else { return }
 			let byte = bytes[offset++]
 			tmpFragment = Base64Decoder.decodeValue(byte)
-		} while (tmpFragment < 0);
+		} while (tmpFragment < 0)
 		let fragment = UInt8(bitPattern: tmpFragment)
 
 		switch step {
